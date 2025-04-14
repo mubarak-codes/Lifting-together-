fae36aadbe092ad0aa1ca1e3c9451e8fc74979cd
mubarak-codes <mubarakcodes@gmail.com>
2025-04-14 22:18:59 +0100

    update about page

--- src/components/Vision.jsx
+++ src/components/Vision.jsx
@@ -22,6 +22,21 @@ const Vision = ({isHome = false})=>{
      <source src="vid/Our_mission.mp4" type="video/mp4" />
   Your browser does not support the video tag.
     </video>
+
+    <h3 className="ff-rubik text-center mt-5 mb-4">{isHome? '': 'Our Focus Areas:' }</h3>
+    <div className="opacity-75">{isHome? '': `We believe that education, healthcare, and economic empowerment are intricately linked, forming the foundation for a life of dignity, self-sufficiency, and fulfillment.
+By providing access to quality education, we empower women and children to acquire skills, knowledge, and critical thinking abilities necessary to navigate the complexities of life. Education unlocks doors to better healthcare, economic opportunities, and informed decision-making.
+However, education alone is not enough. Access to healthcare services, including nutrition, wellness, and medical care, is essential for women and children to thrive. Healthy individuals are more likely to pursue educational and economic opportunities, breaking cycles of poverty and vulnerability.
+     Economic empowerment is the crucial third pillar, enabling women to secure stable incomes, build assets, and make informed financial decisions. By providing job training, entrepreneurship support, and financial literacy, we help women create better futures for themselves and their families.<br /><br/>
+     <span className="fw-bold">Our holistic approach recognizes that these three pillars are:</span><br />
+     - Education informs healthcare choices and economic opportunities.<br />
+     - Healthcare enables individuals to pursue education and economic goals.< br />
+     - Economic empowerment provides the stability to invest in education and healthcare.<br /><br />
+     <span className="fw-bold">By addressing these interconnected needs,we creates a comprehensive support system that empowers women and children to:</span><br />
+     - Break cycles of poverty and inequality<br />
+     - Build resilience and self-sufficiency<br />
+     - Thrive in their communities<br />
+     Together, we can unlock the potential of women and children, fostering a brighter, more equitable future for all.`}</div>
    </section> 
   </>
  )
