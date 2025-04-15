59d477b65f4b78fad3fcf54d45941099bc032993
mubarak-codes <mubarakcodes@gmail.com>
2025-04-15 18:01:30 +0100

    cbange nav item display

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,13 +10,17 @@ const Navbar = ()=>{
     <nav className="navbar navbar-expand-sm border-bottom py-1 px-3 bg-white">
      <Link to="/" className="navbar-brand w-20"> <img src="img/lifting_together_trans.png" className="w-30-sm w-70" /><span className="d-none d-sm-inline-block ff-josefin-sans fw-bolder">LIFTING TOGETHER</span></Link>
 
-     <i class="fa-solid fa-bars mfs-6 d-sm-none click-grow navbar-toggler" data-bs-toggle="collapse" data-bs-target="#nav"></i>
-     <div className="collapse navbar-collapse ms-auto" id="nav">
-      <div className="ms-auto navbar-nav gap-3 mfs-3">
-      <Link to="/" className="nav-item text-black ms-auto click-fade">Home</Link>
-      <Link to="/about" className="nav-item text-black ms-auto click-fade">About</Link>
-      <Link to="/contact" className="nav-item text-black ms-auto click-fade">Contact</Link>
-      <Link to="/contact" className="nav-item text-black ms-auto click-fade">Donate</Link>
+     <i class="fa-solid fa-bars mfs-6 d-sm-none click-grow navbar-toggler" data-bs-toggle="offcanvas" data-bs-target="#nav"></i>
+     <div className="offcanvas offcanvas-end w-100 " id="nav">
+      <div className="offcanvas-header border-bottom">
+      <h3 className="offcanvas-title ff-josefin-sans fw-bolder">LIFTING TOGETHER</h3> 
+       <i class="fa-solid fa-xmark ms-auto mfs-8 fw-ligter" data-bs-dismiss="offcanvas"></i>
+      </div>
+      <div className=" navbar-nav gap-3 mfs-3 offcanvas-body">
+      <Link to="/" className="nav-item text-black click-fade">Home</Link>
+      <Link to="/about" className="nav-item text-black click-fade">About</Link>
+      <Link to="/contact" className="nav-item text-black click-fade">Contact</Link>
+      <Link to="/contact" className="nav-item text-black click-fade">Donate</Link>
       </div>
      </div>
     </nav>
