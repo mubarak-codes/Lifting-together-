516de7cdfe8dfcf7889a7a04b17f551a7a6dec6c
mubarak-codes <mubarakcodes@gmail.com>
2025-04-15 13:33:01 +0100

    created contact page

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,10 +1,21 @@
+import {useNavigate} from 'react-router-dom';
 
 
 const ContactPage = ()=>{
 
  return(
   <>
-   <section className="p-4 p-sm-5">
+   <section className="p-4 p-sm-5 mt-5">
+    <h1 className="ff-rubik mt-3 mb-4">Contact</h1>
+    <div className="mb-3">To contact the orgsnisation, email us:</div>
+    <a href="mailto:letlifttogether@gmail.com" className="mb-4 btn btn-danger rounded-3 click-grow click-fade px-3 py-2">
+     Email us
+    </a>
+
+    <div className="mb-3">To donate, email us</div>
+    <a href="mailto:letlifttogether@gmail.com" className="btn btn-danger rounded-3 click-grow click-fade px-3 py-2">
+     Donate
+    </a>
    </section>
   </>
  )
